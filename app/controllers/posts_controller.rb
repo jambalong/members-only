f559d6f817d23d98437359cfd7a7e26035d8c379
John Alvin Ambalong <119690431+jambalong@users.noreply.github.com>
2024-09-24 01:11:31 -0700

    Add Posts#index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,19 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
   end
 
   def new
+    @post = Post.new(post_params)
   end
 
   def create
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
